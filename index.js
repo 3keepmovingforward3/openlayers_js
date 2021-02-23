8701e386e94251bfc4a8c56ddbe771970bf737cd
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2021-02-23 08:37:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
+import MousePosition from 'ol/control/MousePosition';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import {createStringXY} from 'ol/coordinate';
+import {defaults as defaultControls} from 'ol/control';
+import {fromLonLat} from 'ol/proj';
 
-const map = new Map({
-  target: 'map',
+var mousePositionControl = new MousePosition({
+  coordinateFormat: createStringXY(4),
+  projection: 'EPSG:4326',
+  // comment the following two lines to have the mouse position
+  // be placed within the map.
+  className: 'custom-mouse-position',
+  target: document.getElementById('mouse-position'),
+  undefinedHTML: '&nbsp;',
+});
+
+var map = new Map({
+  controls: defaultControls().extend([mousePositionControl]),
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
-  ],
+      source: new OSM(),
+    }) ],
+  target: 'map',
   view: new View({
-    center: [0, 0],
-    zoom: 0
-  })
+    maxZoom: 25,
+    center: fromLonLat([-75.1652,39.9526]), // Philadelphia
+    zoom: 9,
+  }),
+});
+
+var projectionSelect = document.getElementById('projection');
+projectionSelect.addEventListener('change', function (event) {
+  mousePositionControl.setProjection(event.target.value);
+});
+
+var precisionInput = document.getElementById('precision');
+precisionInput.addEventListener('change', function (event) {
+  var format = createStringXY(event.target.valueAsNumber);
+  mousePositionControl.setCoordinateFormat(format);
 });
